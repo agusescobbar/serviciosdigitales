4ae5c1fd8531f4b5312948e0d1a46588162796f5
agusescobbar <panmmnda@gmail.com>
2025-06-29 21:08:18 -0300

    scrolltriger

--- main.js
+++ main.js
@@ -4,28 +4,54 @@ document.addEventListener("DOMContentLoaded", () => {
   if (foto) {
     foto.addEventListener("mouseenter", () => {
       foto.src = "imag/yo-hover.webp";
+      gsap.from(foto,{
+        rotate:5,
+        x: 5,
+        ease: "back.out",
+      })
     });
 
     foto.addEventListener("mouseleave", () => {
       foto.src = "imag/yo.webp";
+      gsap.from(foto,{
+        rotate:0,
+        x: 0,
+        ease: "back.out",
+      })
     });
   }
 });
 
 let logo = document.querySelector(".logo");
 
+
 gsap.from(logo,{
   x : -200,
-  rotate : 90,
   duration : 2,
   delay : 0.5,
   ease : "back.out"
 })
 
 gsap.from("li", {
-  y: -60,
+  x: 200,
   duration: 2,
   delay: 0.5,
-  rotate : 25,
-  ease: "back.out"
+  ease: "back.out",
 })
+
+gsap.registerPlugin(ScrollTrigger);
+
+//animacion cajas con texto
+gsap.from(".servicio",{
+  y: 150,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: ".servicio",
+  })
+
+gsap.from(".container-contenido",{
+  y: 150,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: ".container-contenido",
+  })
